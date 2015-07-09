64661f53daea139621e0c34e65f59215618ebd09
Ben Moon <guiltydolphin@gmail.com>
2015-07-09 09:47:55 +0100

    Add functionally dependent operation classes

--- Angle/Parse/Parser.hs
+++ Angle/Parse/Parser.hs
@@ -1,3 +1,18 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Angle.Parse.Parser
     (
     ) where
+
+import Angle.Lex.Lexer
+import Control.Monad.Reader
+import Control.Monad.Error
+    
+
+newtype LangEnv = Env [(Expr, Expr)]
+    
+class LangAdd a b c | a b -> c where
+    langAdd :: a -> b -> c
+
+instance (Num a) => LangAdd a a a where
+    langAdd = (+)
