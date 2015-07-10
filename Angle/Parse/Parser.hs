2b024d232cddfc3e813660f10d5d3e4c982ccf51
Ben Moon <guiltydolphin@gmail.com>
2015-07-10 14:31:53 +0100

    Add types for Angle types

--- Angle/Parse/Parser.hs
+++ Angle/Parse/Parser.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 module Angle.Parse.Parser
     (
     ) where
@@ -12,3 +13,22 @@ data LangError = TypeError Expr
                | SyntaxError String
                | UndefinedIdent String
                | DefaultError String
+
+                       
+                 
+data LangType = LList | LBool | LStr | LInt | LFloat
+                deriving (Eq)
+              
+typeOf :: LangLit -> LangType
+typeOf (LitStr _) = LStr
+typeOf (LitList _) = LList
+typeOf (LitInt _) = LInt
+typeOf (LitFloat _) = LFloat
+typeOf (LitBool _) = LBool
+              
+instance Show LangType where
+    show LList = "list"
+    show LBool = "boolean"
+    show LStr = "string"
+    show LInt = "integer"
+    show LFloat = "float"
