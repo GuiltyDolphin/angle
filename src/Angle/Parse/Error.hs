96c82ef72a29f565fbb643ffb62d1775227dcbe8
Ben Moon <guiltydolphin@gmail.com>
2015-07-12 16:49:17 +0100

    Move error system to separate module

---
+++ src/Angle/Parse/Error.hs
@@ -0,0 +1,85 @@
+module Angle.Parse.Error
+    ( typeMismatchErr
+    , typeUnexpectedErr
+    , typeNotValidErr
+    , nameNotDefinedErr
+    , nameNotFunctionErr
+    , nameNotValueErr
+    , LangError
+    ) where
+
+import Angle.Types.Lang
+
+import Control.Monad.Error
+
+data LangError = TypeError TypeError
+               | SyntaxError String
+               | NameError NameError
+               | DefaultError String
+                 
+typeErr = TypeError
+syntaxErr = SyntaxError
+nameErr = NameError
+defaultErr = DefaultError
+
+instance Show LangError where
+    show (TypeError e) = "wrong type in expression: " ++ show e
+    show (SyntaxError s) = "syntax error: " ++ s
+    show (NameError v) = "name error: " ++ show v
+    show (DefaultError s) = "defaultError: " ++ s
+
+instance Error LangError where
+    noMsg = DefaultError ""
+    strMsg = DefaultError
+
+data TypeError = TypeMismatch   LangType LangType
+               | TypeUnexpected LangType LangType
+               | TypeNotValid   LangType
+                 
+typeMismatchErr   t1 = typeErr . TypeMismatch   t1
+typeUnexpectedErr t1 = typeErr . TypeUnexpected t1
+typeNotValidErr      = typeErr . TypeNotValid
+               
+instance Show TypeError where
+    show (TypeMismatch l r) = "type mismatch: got (" ++ show l ++ ", " ++ show r ++ ") but both types should be the same"
+    show (TypeUnexpected l r) = "unexpected type: " ++ show l ++ ", expecting: " ++ show r
+    show (TypeNotValid l) = "type not valid for scenario: " ++ show l
+                            
+data NameError = NameNotDefined LangIdent 
+               | NameNotFunction LangIdent
+               | NameNotValue LangIdent
+
+nameNotDefinedErr, nameNotFunctionErr, nameNotValueErr :: LangIdent -> LangError
+nameNotDefinedErr  = nameErr . NameNotDefined
+nameNotFunctionErr = nameErr . NameNotFunction
+nameNotValueErr    = nameErr . NameNotValue
+
+instance Show NameError where
+    show (NameNotDefined  name) = "not in scope: "         ++ name
+    show (NameNotFunction name) = "not a valid function: " ++ name
+    show (NameNotValue    name) = "no value assigned: "    ++ name
+                               
+
+
+data LError = LError { errorErr    :: LangError  -- The actual error
+                     , errorSource :: String
+                     , lerrorPos   :: (Int, Int, Int) -- Position at which the error occurred
+                     }
+
+instance Show LError where
+    -- show (LError { errorExpr=ex, errorStmt=es, errorErr=ee })
+    --     = unlines
+    --       [ "error in statement: " ++ show es
+    --       , "in expression: " ++ show ex
+    --       , show ee ]
+    show (LError { errorErr=ee, lerrorPos=ep@(_,_,pos), errorSource=es })
+        = cEp ++ cEt ++ cEe
+          where cEp = showPos ep ++ "\n"
+                cEt = takeWhile (/='\n') (drop pos es)
+                cEe = show ee
+                showPos (ln,cn,_) =
+                  concat ["line: ", show ln, ", column: ", show cn]
+                         
+instance Error LError where
+    noMsg = LError {errorErr=noMsg, lerrorPos=(0,0,0), errorSource=""}
+    strMsg m = noMsg {errorErr=strMsg m}
--- src/Angle/Parse/Parser.hs
+++ src/Angle/Parse/Parser.hs
@@ -7,6 +7,8 @@ module Angle.Parse.Parser
 
 import Angle.Lex.Lexer
 import Angle.Lex.Helpers
+import Angle.Parse.Error
+
 import Control.Monad.Reader
 import Control.Monad.Error
 import Control.Applicative
@@ -14,42 +16,11 @@ import Control.Monad.State
 import qualified Data.Map as M
 import Debug.Trace (trace)
     
-import Data.IORef
-
-
-data LangError = TypeError TypeError
-               | SyntaxError String
-               | UndefinedIdent String
-               | DefaultError String
-
-instance Show LangError where
-    show (TypeError e) = "wrong type in expression: " ++ show e
-    show (SyntaxError s) = "syntax error: " ++ s
-    show (UndefinedIdent v) = "identifier not defined: " ++ v
-    show (DefaultError s) = "defaultError: " ++ s
-
-instance Error LangError where
-    noMsg = DefaultError ""
-    strMsg = DefaultError
-
-type TestInt = IORef Int
-  
-type Env = IORef [(String, Expr)] 
-
-type EnvE = ErrorT LangError IO    
-newtype IOLangError a = ILE { runIOLangError :: ErrorT LangError IO a }
-    deriving (Functor, Applicative, Monad, MonadError LangError)
-           
-type LE = ErrorT LangError (ReaderT Env IO)
-
-
-opMap = [(OpNot, langNot)]
-
 langNot (LitBool x) = LitBool (not x)
 
 langLitJoin :: LangLit -> LangLit -> Either LangError LangLit
 langLitJoin (LitList xs) (LitList ys) = return $ LitList (xs++ys)
-langLitJoin x (LitList _) = Left (TypeError $ TypeUnexpected (typeOf x) LList)
+langLitJoin x (LitList _) = Left (typeUnexpectedErr (typeOf x) LTList)
 langLitJoin l@(LitList _) x = langLitJoin x l
 
                             
@@ -60,63 +31,17 @@ langLitAdd (LitFloat x) r
     = case r of
         LitInt y -> return $ LitFloat (x + fromIntegral y)
         LitFloat y -> return $ LitFloat (x + y)
-        _ -> Left . TypeError $ TypeUnexpected (typeOf r) LFloat
+        _ -> Left $ typeUnexpectedErr (typeOf r) LTFloat
 langLitAdd l r@(LitFloat _) = langLitAdd r l
 langLitAdd l r 
     | typeOf l /= typeOf r 
-        = Left . TypeError $ TypeMismatch ltype rtype
+        = Left $ typeMismatchErr ltype rtype
     | otherwise 
-        = Left . TypeError $ TypeNotValid ltype
+        = Left $ typeNotValidErr ltype
     where ltype = typeOf l
           rtype = typeOf r
                  
-data LangType = LList | LBool | LStr | LInt | LFloat
-                deriving (Eq)
-              
-typeOf :: LangLit -> LangType
-typeOf (LitStr _) = LStr
-typeOf (LitList _) = LList
-typeOf (LitInt _) = LInt
-typeOf (LitFloat _) = LFloat
-typeOf (LitBool _) = LBool
-              
-instance Show LangType where
-    show LList = "list"
-    show LBool = "boolean"
-    show LStr = "string"
-    show LInt = "integer"
-    show LFloat = "float"
-
-data TypeError = TypeMismatch LangType LangType
-               | TypeUnexpected LangType LangType
-               | TypeNotValid LangType
-               
-instance Show TypeError where
-    show (TypeMismatch l r) = "type mismatch: got (" ++ show l ++ ", " ++ show r ++ ") but both types should be the same"
-    show (TypeUnexpected l r) = "unexpected type: " ++ show l ++ ", expecting: " ++ show r
-    show (TypeNotValid l) = "type not valid for scenario: " ++ show l
-
-
-data LError = LError { errorExpr :: Expr
-                     , errorStmt :: Stmt
-                     , errorErr :: LangError 
-                     , errorSource :: String
-                     , lerrorPos :: (Int, Int, Int)
-                     }
-
-instance Show LError where
-    -- show (LError { errorExpr=ex, errorStmt=es, errorErr=ee })
-    --     = unlines
-    --       [ "error in statement: " ++ show es
-    --       , "in expression: " ++ show ex
-    --       , show ee ]
-    show (LError { errorErr=ee, lerrorPos=ep@(_,_,pos), errorSource=es })
-        = cEp ++ cEt ++ cEe
-          where cEp = showPos ep ++ "\n"
-                cEt = takeWhile (/='\n') (drop pos es)
-                cEe = show ee
-                showPos (ln,cn,_) =
-                  concat ["line: ", show ln, ", column: ", show cn]
+    
 
 -- **************
 -- Evaluating
@@ -170,21 +95,21 @@ getVar :: Ident -> EvalCxt (Maybe LangLit, Maybe CallSig)
 getVar name = do
   env <- get
   case M.lookup name env of
-    Nothing -> throwError $ "not in scope: " ++ name
+    Nothing -> throwError . nameNotDefinedErr $ name
     Just x -> return x
     
 getVarVal :: Ident -> EvalCxt LangLit
 getVarVal name = do
   (e,_) <- getVar name
   case e of
-    Nothing -> throwError $ "no value definition for " ++ name
+    Nothing -> throwError . nameNotValueErr $ name
     Just v -> return v
 
 getFunVal :: Ident -> EvalCxt CallSig
 getFunVal name = do
   (_,f) <- getVar name
   case f of
-    Nothing -> throwError $ "no function definition: " ++ name
+    Nothing -> throwError . nameNotFunctionErr $ name
     Just x -> return x
 
               
@@ -211,7 +136,8 @@ addOp (ExprLit x) (ExprLit y) = return $ addLit x y
 addOp x@(ExprIdent _) r = do
   l <- reduceExprToLit x
   addOp (ExprLit l) r
-addOp x y = throwError "wrong types in addition"
+-- TODO: Make this into a proper type error
+addOp x y = throwError noMsg
         
 negOp :: Expr -> EvalCxt Expr
 negOp (ExprLit x) = liftM ExprLit (negLit x)
@@ -219,7 +145,7 @@ negOp x = execExpr x >>= negOp
           
 negLit :: LangLit -> EvalCxt LangLit
 negLit (LitInt x) = return $ LitInt (-x)
-negLit _ = throwError "wrong type in negation!"
+negLit x = throwError . typeNotValidErr . typeOf $ x
 
                                 
 -- TODO: Version of this that can return errors
