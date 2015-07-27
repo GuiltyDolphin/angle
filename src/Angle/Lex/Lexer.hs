06f564526202b5b71f1629182bffd9ab074b8d6a
Ben Moon <guiltydolphin@gmail.com>
2015-07-27 17:56:08 +0100

    Cleanup lexer file

--- src/Angle/Lex/Lexer.hs
+++ src/Angle/Lex/Lexer.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
-{-# LANGUAGE FunctionalDependencies #-}
 module Angle.Lex.Lexer 
     ( program
     , stmt
@@ -22,39 +21,35 @@ module Angle.Lex.Lexer
     , structDefun
     ) where
 
--- Write this!
-import Angle.Lex.Helpers
-import Angle.Types.Lang
-import Angle.Lex.Token
-import Control.Monad.State
+
 import Control.Applicative
+import Control.Monad.State
+
+import Angle.Lex.Helpers
+import Angle.Lex.Token
+import Angle.Types.Lang
 
 
 stmt :: Scanner Stmt
 stmt = (multiStmt <|> singleStmt) <?> "statement"
        
--- singleStmt :: Scanner Stmt
--- singleStmt = liftM SingleStmt singStmt
-             
+
 singleStmt :: Scanner Stmt
 singleStmt = do
   initPos <- liftM sourcePos get
   res <- singStmt
   endPos <- liftM sourcePos get
-  return SingleStmt { stmtSingStmt = res
-                    , stmtSourcePos = SourceRef (initPos, endPos)
-                    }
+  return SingleStmt 
+             { stmtSingStmt  = res
+             , stmtSourcePos = SourceRef (initPos, endPos)
+             }
 
--- |Statement consisting of zero or more statements
--- >>> evalScan "{}" multiStmt
--- Right (...[])
---
--- >>> evalScan "{1;3}" multiStmt
--- Right (...[...1...3...])
---
+
 -- TODO: Is it wise to allow empty multi-statements?
+-- | Statement consisting of zero or more statements.
 multiStmt :: Scanner Stmt
 multiStmt = MultiStmt <$> within tokMultiStmtStart tokMultiStmtEnd (many stmt)
+
 
 -- TODO: Last statement in a multi-statement block, or at
 -- end of file, shouldn't need to have a newline or semi-colon
@@ -66,18 +61,20 @@ singStmt = stmtComment
            <|> stmtExpr   <* singStmtEnd
            <?> "statement"
 
+
+singStmtEnd :: Scanner ()
 singStmtEnd = surrounded whitespace (void (char ';')) <?> "end of statement"
               -- <|> void (char '\n')
               -- <|> void (lookAhead tokMultiStmtEnd)
               -- <|> lookAhead tokEOF
                
--- |Variable assignment
--- >>> evalScan "x=5" stmtAssign
--- Right (..."x"...5...)
+
+-- | Variable assignment.
 stmtAssign :: Scanner SingStmt
 stmtAssign = StmtAssign 
              <$> tryScan (langIdent <* tokAssign) 
              <*> expr
+
 
 stmtComment :: Scanner SingStmt
 stmtComment = StmtComment 
@@ -88,12 +85,15 @@ stmtComment = StmtComment
                 -- <|> void (string "-#")
                 -- <?> "end of comment"
                   
+
 stmtStruct :: Scanner SingStmt 
 stmtStruct = liftM StmtStruct langStruct
+
 
 stmtReturn :: Scanner SingStmt
 stmtReturn = liftM StmtReturn (string "return " *> expr) 
                <?> "return construct"
+
 
 stmtExpr :: Scanner SingStmt
 stmtExpr = liftM StmtExpr expr
@@ -107,9 +107,7 @@ langStruct =     structFor
              <?> "language construct"
              
              
--- |For loop
--- >>> evalScan "for x in (2..8) do {}" structFor
--- Right (..."x"...2...8...[]...)
+-- | For loop.
 structFor :: Scanner LangStruct
 structFor = do
   string "for "
@@ -120,20 +118,15 @@ structFor = do
   body <- stmt
   return $ StructFor name iter body
 
--- |While loop
--- >>> evalScan "while true do {}" structWhile
--- Right (...True...[]...)         
+
+-- | While loop.
 structWhile :: Scanner LangStruct
 structWhile = StructWhile 
               <$> (string "while " *> expr)
               <*> (string " do "   *> stmt)
          
--- |Conditional if statement
--- >>> evalScan "if true then {}" structIf
--- Right (...True...[]...Nothing)
---
--- >>> evalScan "if false then {} else {}" structIf
--- Right (...False...[]...Just...[]...)
+
+-- | Conditional if statement.
 structIf :: Scanner LangStruct
 structIf = StructIf
            <$> (string "if " *> expr)
@@ -141,28 +134,24 @@ structIf = StructIf
            <*> optional (string "else " *> stmt)
 
         
--- |Function definition
--- >>> evalScan "defun foo(x) { print(x) }" structDefun
--- Right (..."foo"..."x"...)
+-- | Function definition.
 structDefun :: Scanner LangStruct
 structDefun = StructDefun
               <$> (string "defun " *> identName)
               <*> (CallSig 
-                   <$> callList' <* tokStmtBetween
+                   <$> callList <* tokStmtBetween
                    <*> stmt)
+
 
 program :: Scanner Stmt
 program = liftM MultiStmt $ followed tokEOF (many stmt)
           
-program' = liftM MultiStmt $ followed tokEOF (some stmt)
-  
-  -- sepWith (some tokNewLine) stmt
 
+exprLit :: Scanner Expr
 exprLit = liftM ExprLit langLit
-
                
 
--- |Language literals
+-- | Language literals.
 langLit :: Scanner LangLit
 langLit = litStr 
           <|> litNull
@@ -173,75 +162,53 @@ langLit = litStr
           <|> litInt 
           <?> "literal"
 
--- |A literal string
--- >>> evalScan "\"test\"" litStr
--- Right (... "test")
---
--- >>> evalScan "test" litStr
--- Left ...
--- ...
+
+-- | A literal string.
 litStr :: Scanner LangLit
 litStr = liftM LitStr tokString <?> "string literal"
 
--- |Denary integer
--- >>> evalScan "123" litInt
--- Right (... 123)
---
--- >>> evalScan "test" litInt
--- Left ...
--- ...
+
+-- | Denary integer.
 litInt :: Scanner LangLit
 litInt = liftA LitInt tokInt <?> "integer literal"
--- litInt = liftM (LitInt . read) (some tokDenaryDigit) <?> "integer literal"
          
 
--- |Floating-point literal
--- >>> evalScan "12.3" litFloat
--- Right (...12.3)
+-- | Floating-point literal.
 litFloat :: Scanner LangLit
 litFloat = liftM LitFloat tokFloat 
            <?> "floating-point literal"
              
 
--- |Multi-type list
--- >>> evalScan "[1,\"hello\",true]" litList
--- Right (... [... 1...,... "hello"...,... True...])
---
--- >>> evalScan "1,\"hello\",true" litList
--- Left ...
--- ...
+-- | Multi-type list.
 litList :: Scanner LangLit
 litList = liftM LitList (tokList $ sepWith tokEltSep langLit) 
           <?> "list literal"
               
+
 exprList :: Scanner Expr
 exprList = liftM ExprList (tokList $ sepWith tokEltSep expr)
 
 
--- |Boolean literal
--- >>> evalScan "true" litBool
--- Right (... True)
---
--- >>> evalScan "false" litBool
--- Right (... False)
+-- | Boolean literal.
 litBool :: Scanner LangLit
 litBool = liftM LitBool (litTrue <|> litFalse) 
           <?> "boolean literal"
     where litTrue  = tokTrue  >> return True
           litFalse = tokFalse >> return False
                    
--- |Dotted range of values
--- >>> evalScan "(1..7)" litRange
--- Right (...1...7...)
---
+
 -- TODO: Add additional `step' to ranges (1..7..3)
+-- | Dotted range of values.
 litRange = parens (LitRange 
                    <$> (expr <* tokRangeSep) 
                    <*> expr)
            <?> "range literal"
                
--- | Non-valued literal
+
+-- | Non-valued literal.
+litNull :: Scanner LangLit
 litNull = string "()" <|> string "null" >> return LitNull
+
 
 expr :: Scanner Expr
 expr = (   tryScan exprLit
@@ -253,106 +220,132 @@ expr = (   tryScan exprLit
        <|> exprIdent)
        <?> "expression"
        
+
+exprIdent :: Scanner Expr
 exprIdent = liftM ExprIdent langIdent <?> "identifier"
            
 
 identName :: Scanner LangIdent
 identName = liftM LangIdent (tryScan ident) <?> "identifier"
             
+
+langIdent :: Scanner LangIdent
 langIdent = identName
             
+
 funIdent :: Scanner LangIdent
 funIdent = char '$' *> identName
            
+
 exprFunIdent :: Scanner Expr
 exprFunIdent = liftM ExprFunIdent funIdent
          
+
+exprLambda :: Scanner Expr
 exprLambda = liftM ExprLambda $ do
   char '('
-  args <- callList'
+  args <- callList
   tokSpace
   body <- stmt
   char ')'
   return $ CallSig args body
-  
 
-
--- langIdent :: Scanner LangIdent
--- langIdent = tryScan (liftM LangIdent ident) <?> "identifier"
-
--- data LangFunCall = FC LangIdent [Expr]
---                    deriving (Show)
-                 
 
 -- | Set of arguments for a function 
+arglist :: Scanner [Expr]
 arglist = within tokTupleStart tokTupleEnd (sepWith tokEltSep expr)
 
-callList' = do
+
+callList :: Scanner ArgSig
+callList = do
   tokTupleStart
   params <- sepWith tokEltSep identName
   catcher <- optional (string ".." *> identName)
   tokTupleEnd
   return $ ArgSig params catcher
   
-  
 
--- exprFunCall = liftM ExprFunCall langFunCall
-exprFunCall = ExprFunCall <$> tryScan (langIdent <* lookAhead (char '(')) <*> arglist <?> "function call"
+exprFunCall :: Scanner Expr
+exprFunCall = ExprFunCall 
+              <$> tryScan (langIdent <* lookAhead (char '(')) 
+              <*> arglist 
+              <?> "function call"
               
+
+exprOp :: Scanner Expr
 exprOp = liftM ExprOp langOp -- <?> "operation"
                        
 
--- langOp = unOp <|> binOp <?> "operation"
+langOp :: Scanner LangOp
 langOp = specOp <|> multiOp -- <?> "operation"
 
-opMult, opDiv, opAdd, opSub, opNot :: Scanner Op
+
 makeOp :: Scanner a -> Op -> Scanner Op
 makeOp sc op = sc >> return op
+
 
 -- TODO: This conflicts with literal -ve for integers,
 -- either make it so that literals use the operator (probably
 -- slower) or resolve the conflict!
-opNeg  = makeOp (char '-')    OpNeg  <?> "operator (-)"
-opMult = makeOp (char '*')    OpMult <?> "operator (*)"
-opDiv  = makeOp (char '/')    OpDiv  <?> "operator (/)"
-opAdd  = makeOp (char '+')    OpAdd  <?> "operator (+)"
-opSub  = makeOp (char '-')    OpSub  <?> "operator (-)"
-opNot  = makeOp (char '^')    OpNot  <?> "operator (^)"
-opGreater = makeOp (char '>') OpGreater <?> "operator (>)"
-opLess = makeOp (char '<')    OpLess <?> "operator (<)"
-opGreaterEq = makeOp (string ">=") OpGreaterEq <?> "operator (>=)"
-opLessEq = makeOp (string "<=") OpLessEq <?> "operator (<=)"
-opEq   = makeOp (string "==") OpEq   <?> "operator (==)"
-opOr   = makeOp (char '|')    OpOr   <?> "operator (|)"
-opAnd  = makeOp (char '&')    OpAnd <?> "operator (&)"
+-- Probably fixed.
 
+opAdd, opAnd, opDiv, opEq,
+  opGreater, opGreaterEq, opLess, opLessEq,
+  opMult, opNeg, opNot, opOr, opSub
+  :: Scanner Op
+opAdd  = makeOp (char '+')    OpAdd
+opAnd  = makeOp (char '&')    OpAnd
+opDiv  = makeOp (char '/')    OpDiv
+opEq   = makeOp (string "==") OpEq
+opGreater = makeOp (char '>') OpGreater
+opGreaterEq = makeOp (string ">=") OpGreaterEq
+opLess = makeOp (char '<')    OpLess
+opLessEq = makeOp (string "<=") OpLessEq
+opMult = makeOp (char '*')    OpMult
+opNeg  = makeOp (char '-')    OpNeg
+opNot  = makeOp (char '^')    OpNot
+opOr   = makeOp (char '|')    OpOr
+opSub  = makeOp (char '-')    OpSub
+
+
+userOp :: Scanner Op
 userOp = liftM (UserOp . LangIdent) (some tokOpChar) <?> "operator"
+
        
--- |Operators that can be used outside parentheses
--- >>> evalScan "^true" specOp
--- Right (...Not...True...)
+-- | Special operators that can be used outside of
+-- parentheses in a prefix form.
 specOp :: Scanner LangOp
 specOp = choice (map preOp specOps) <?> "special operator"
                        
+
 specOps :: [Scanner Op]
-specOps = [opNot, opNeg]
+specOps = [opNeg, opNot]
           
+
+preOp :: Scanner Op -> Scanner LangOp
 preOp sc = do
   op  <- sc
   opr <- expr
   return $ SpecOp op opr
          
+
 -- |Operators called within parentheses that may have
 -- multiple operands
--- >>> evalScan "(+ 1 3)" multiOp
--- Right (...Add...1...3...)
 multiOp :: Scanner LangOp
-multiOp = parens (choice (map multOp multiOps) 
+multiOp = parens (tryScan (choice (map multOp multiOps))
                   <|> multOp userOp) <?> "operator expression"
           
+
+-- | List of all the MultiOp scanners.
 multiOps :: [Scanner Op]
-multiOps = [opMult, opDiv, opAdd, opSub, opEq, opOr]
+multiOps = [ opAdd, opAnd
+           , opDiv, opEq
+           , opGreater, opGreaterEq
+           , opLess, opLessEq
+           , opMult, opOr
+           , opSub ]
            
+
 multOp :: Scanner Op -> Scanner LangOp
 multOp sc = MultiOp
             <$> (sc <* tokSpace)
