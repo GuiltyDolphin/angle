0ef97b21753cb2ca144b588408eeda127d275492
Ben Moon <guiltydolphin@gmail.com>
2015-07-30 13:04:29 +0100

    Improve REPL and interpreter functionality
     * Improves support for running files.
     * Improves option parsing.
     * Add basic support for multi-statements in REPL.
     * Allow files to be run interactively.

--- src/Angle/Parse/Interp.hs
+++ src/Angle/Parse/Interp.hs
@@ -3,6 +3,7 @@ module Angle.Parse.Interp
     ) where
 
 -- import System.IO
+import Control.Monad
 import System.Environment
 import System.Exit
 
@@ -19,19 +20,38 @@ stringToStmt s = evalScan s program
 runStmt :: String -> Stmt -> IO (Either LError LangLit)
 runStmt source s = runExecIOEnv (basicEnv {sourceText=source}) (execStmt s)
             
-runFile :: FilePath -> IO ()
-runFile f = do
+runFile :: Bool -> FilePath -> IO ()
+runFile showSyntx f = do
   s <- readFile f
   case stringToStmt s of
     Left err -> putStrLn "Syntax error" >> print err >> exitWith (ExitFailure 100)
     Right r -> do
+              when showSyntx (do
+                  putStrLn "Here's what I think..."
+                  putStrLn (showSyn r))
               res <- runStmt s r
               case res of
                 Left err -> putStrLn "Runtime error" >> print err >> exitWith (ExitFailure 101)
                 Right _ -> return ()
                              
-interp :: [String] -> IO ()
-interp (f:_) = runFile f
+interp :: Options -> IO ()
+interp (Options { showRep=sr, fileName=f }) = runFile sr f
+               
+
+data Options = Options { showRep :: Bool
+                       , fileName :: FilePath }
+               deriving (Show, Eq)
+                        
+
+basicOptions :: Options
+basicOptions = Options { showRep = False
+                       , fileName = "" }
+
+
+argsToOpts :: [String] -> Options
+argsToOpts xs | "--show-rep" `elem` xs = (argsToOpts xs') { showRep = True }
+              where xs' = filter (/="--show-rep") xs
+argsToOpts [f] = basicOptions { fileName = f }
 
 main :: IO ()
-main = getArgs >>= interp
+main = liftM argsToOpts getArgs >>= interp
--- src/Angle/Parse/REPL.hs
+++ src/Angle/Parse/REPL.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveDataTypeable #-}
 module Angle.Parse.REPL 
     ( main 
     ) where
@@ -5,46 +6,123 @@ module Angle.Parse.REPL
 import Control.Monad
 import Control.Monad.State
 import Control.Monad.Error
+import Data.Char (isSpace)
+import Data.List (elemIndices)
+
+import System.Console.CmdArgs hiding (program)
+import qualified System.Console.CmdArgs as CA
 
 import Angle.Lex.Lexer
 import Angle.Lex.Helpers
 import Angle.Parse.Exec
 import Angle.Parse.Error
 import Angle.Types.Lang
+    
+data ReplOptions = ReplOptions
+    { file :: String
+    } deriving (Show, Data, Typeable)
+                 
+
+replOptions :: ReplOptions
+replOptions = ReplOptions
+              { file = def &= typFile
+              }
+              &= CA.program "angle-repl"
+              &= summary "repl for angle programming language"
+
 
 processLine :: String -> IO ()
-processLine s = case evalScan s program of
-                  Left err -> print err
-                  Right res -> runExecIOBasic (execStmt res) >>= print
+processLine s = do
+  case evalScan s program of
+    Left err -> print err
+    Right res -> runExecIOBasic (execStmt res) >>= print
                                
 -- execLine :: Env -> String -> IO Env
 -- execLine e s = case evalScan s program of
 --                  Left err -> print err
 --                  Right res -> runExecIOEnv (execStmt e) res >>= print
-                              
+
+collectLine :: String -> Int -> ExecIO String
+collectLine s multi =
+  if multi + incMultiBy s > 0
+  then do
+    nxt <- liftIO getLine
+    liftM (s++) $ collectLine nxt (multi + incMultiBy s)
+  else return s
+  -- then return s
+  -- else do
+  --    nxt <- liftIO getLine
+  --    if shouldDecMulti s 
+  --    then liftM (s++) $ collectLine nxt (multi-1)
+  --    else if shouldIncMulti s
+  --         then liftM (s++) $ collectLine nxt (multi+1)
+  --         else liftM (s++) $ collectLine nxt multi
+
 runLine :: String -> ExecIO ()
-runLine s = case evalScan s stmt of
+runLine s = do 
+  r <- collectLine s 0
+  case evalScan r stmt of
               Left err -> liftIO $ print err
               Right res -> do
                 toPrint <- execStmt res `catchError` (\e -> liftIO (print e) >> throwError e)
                 liftIO $ printSyn toPrint
 
+                 
+shouldIncMulti :: String -> Bool
+shouldIncMulti xs = count '{' xs > count '}' xs
+                    
+incMultiBy :: String -> Int
+incMultiBy xs = count '{' xs - count '}' xs
+
+                    
+
+shouldDecMulti :: String -> Bool
+shouldDecMulti xs = count '}' xs > count '{' xs
+                    
+
+count :: Eq a => a -> [a] -> Int
+count x = length . elemIndices x
+
                        
 printSyn :: (ShowSyn a) => a -> IO ()
 printSyn = putStrLn . showSyn
 
+
+withSource :: String -> ExecIO ()
+withSource s = do              
+  case evalScan ('{':s++"}") stmt of
+    Left err -> (liftIO $ print err) >> mainProg
+    Right res -> do
+                 toPrint <- execStmt res `catchError` (\e -> liftIO (print e) >> throwError e)
+                 liftIO $ printSyn toPrint
+                 mainProg
+    
+
+
+mainProg :: ExecIO ()
 mainProg = do
   liftIO $ putStr "> "
   userInput <- liftIO getLine
-  st <- get
-  put st { sourceText=userInput}
   unless (userInput=="exit")
            (do 
              runLine userInput `catchError` (\_ -> mainProg)
              mainProg)
-  
-  
-main = runExecIOBasic mainProg
+                 
+
+runMain :: ReplOptions -> IO ()
+runMain opts = undefined
+               
+runWithSource :: String -> IO ()
+runWithSource s = do
+  x <- runExecIOBasic $ withSource s
+  return ()
+
+main = do 
+  args <- cmdArgs replOptions
+  unless (null $ file args)
+       (readFile (file args) >>= runWithSource)
+  runExecIOBasic mainProg
+  -- runExecIOBasic mainProg
   -- putStr "> "
   -- userInput <- getLine
   -- unless (userInput=="exit") (processLine userInput >> main)
