ebde447d68200f771646b510007e51aa625d7b50
Ben Moon <guiltydolphin@gmail.com>
2015-08-02 12:36:05 +0100

    Improve stack implementation

--- src/Angle/Parse/Types.hs
+++ src/Angle/Parse/Types.hs
@@ -69,7 +69,6 @@ data Env = Env { currentScope :: Scope
                , sourceText :: String
                , envSourceRef :: SourceRef
                , envSynRep :: String
-               , envStack :: Stack
                } deriving (Show, Eq)
 
 -- Stack API
@@ -89,92 +88,92 @@ data Env = Env { currentScope :: Scope
 --   - for error reporting, allows stack depth to be
 --     known.
 
-data Stack = Stack
-    { stackParent :: Maybe Stack
-    , stackStmts :: ([Stmt], [Stmt])
-    , stackLevel :: Int
-    , stackType :: StackType
-    , stackName :: Maybe String
-    } deriving (Show, Eq)
-
-
-data StackType = FunctionStack | LoopStack | GlobalStack
-                 deriving (Show, Eq)
-                          
-
-newStack :: Stack -> StackType -> Maybe String -> Stack
-newStack stack typ name = 
-    let newStmts = 
-            case nextStmt stack of
-              Nothing -> error "newStack: parent has no nextStmt"
-              Just x -> case x of
-                          s@(SingleStmt{}) -> [s]
-                          (MultiStmt xs) -> xs
-    in Stack 
-           { stackParent = Just stack
-           , stackType = typ
-           , stackName = name
-           , stackLevel = stackLevel stack + 1
-           , stackStmts = ([], newStmts) }
-           
-
--- | Update the current stack, retrieving the next
--- statement to be executed.
-forwardStmt :: ExecIO (Maybe Stmt)
-forwardStmt = do
-  stack <- liftM envStack get
-  let stackS = stackStmts stack
-      oldStmts = fst stackS
-      newStmts = snd stackS
-      nextStmt' = take 1 newStmts
-      nextS = case nextStmt' of
-                [] -> Nothing
-                [x] -> Just x
-      newStackStmts = (oldStmts ++ nextStmt', drop 1 newStmts)
-  modifyStack (\s -> s {stackStmts = newStackStmts})
-  return nextS
-         
-
--- | Modify the current stack.
-modifyStack :: (Stack -> Stack) -> ExecIO ()
-modifyStack f = modify (\e -> e {envStack = f $ envStack e})
-         
-
--- | Retrieve the next statement to be executed, if any.
-nextStmt :: Stack -> Maybe Stmt
-nextStmt (Stack { stackStmts = (_,ys) }) 
-    = case take 1 ys of
-        [] -> Nothing
-        [x] -> Just x
-
-
--- execStack :: ExecIO LangLit
--- execStack = do
---   stmt <- forwardStmt
---   case stmt of
---     Nothing -> error "no more statements!"
---     Just x -> execStmt x
-              
-
-localStackWithType :: Stack -> StackType -> Maybe Stack
-localStackWithType s@(Stack { stackType = sTyp }) typ 
-    = if sTyp == typ 
-      then Just s
-      else case stackParent s of
-             Nothing -> Nothing
-             Just par -> localStackWithType par typ
-
-
-getStack :: ExecIO Stack
-getStack = liftM envStack get
-
-
-popStack :: ExecIO ()
-popStack = do
-  stack <- getStack
-  case stackParent stack of
-    Nothing -> throwImplementationErr "popStack: at top stack!"
-    Just x  -> modifyStack (const x)
+-- data Stack = Stack
+--     { stackParent :: Maybe Stack
+--     , stackStmts :: ([Stmt], [Stmt])
+--     , stackLevel :: Int
+--     , stackType :: StackType
+--     , stackName :: Maybe String
+--     } deriving (Show, Eq)
+-- 
+-- 
+-- data StackType = FunctionStack | LoopStack | GlobalStack
+--                  deriving (Show, Eq)
+--                           
+-- 
+-- newStack :: Stack -> StackType -> Maybe String -> Stack
+-- newStack stack typ name = 
+--     let newStmts = 
+--             case nextStmt stack of
+--               Nothing -> error "newStack: parent has no nextStmt"
+--               Just x -> case x of
+--                           s@(SingleStmt{}) -> [s]
+--                           (MultiStmt xs) -> xs
+--     in Stack 
+--            { stackParent = Just stack
+--            , stackType = typ
+--            , stackName = name
+--            , stackLevel = stackLevel stack + 1
+--            , stackStmts = ([], newStmts) }
+--            
+-- 
+-- -- | Update the current stack, retrieving the next
+-- -- statement to be executed.
+-- forwardStmt :: ExecIO (Maybe Stmt)
+-- forwardStmt = do
+--   stack <- liftM envStack get
+--   let stackS = stackStmts stack
+--       oldStmts = fst stackS
+--       newStmts = snd stackS
+--       nextStmt' = take 1 newStmts
+--       nextS = case nextStmt' of
+--                 [] -> Nothing
+--                 [x] -> Just x
+--       newStackStmts = (oldStmts ++ nextStmt', drop 1 newStmts)
+--   modifyStack (\s -> s {stackStmts = newStackStmts})
+--   return nextS
+--          
+-- 
+-- -- | Modify the current stack.
+-- modifyStack :: (Stack -> Stack) -> ExecIO ()
+-- modifyStack f = modify (\e -> e {envStack = f $ envStack e})
+--          
+-- 
+-- -- | Retrieve the next statement to be executed, if any.
+-- nextStmt :: Stack -> Maybe Stmt
+-- nextStmt (Stack { stackStmts = (_,ys) }) 
+--     = case take 1 ys of
+--         [] -> Nothing
+--         [x] -> Just x
+-- 
+-- 
+-- -- execStack :: ExecIO LangLit
+-- -- execStack = do
+-- --   stmt <- forwardStmt
+-- --   case stmt of
+-- --     Nothing -> error "no more statements!"
+-- --     Just x -> execStmt x
+--               
+-- 
+-- localStackWithType :: Stack -> StackType -> Maybe Stack
+-- localStackWithType s@(Stack { stackType = sTyp }) typ 
+--     = if sTyp == typ 
+--       then Just s
+--       else case stackParent s of
+--              Nothing -> Nothing
+--              Just par -> localStackWithType par typ
+-- 
+-- 
+-- getStack :: ExecIO Stack
+-- getStack = liftM envStack get
+-- 
+-- 
+-- popStack :: ExecIO ()
+-- popStack = do
+--   stack <- getStack
+--   case stackParent stack of
+--     Nothing -> throwImplementationErr "popStack: at top stack!"
+--     Just x  -> modifyStack (const x)
 
 
 runExecIOEnv :: Env -> ExecIO a -> IO (Either AngleError a)
@@ -191,4 +190,23 @@ basicEnv = Env { currentScope = emptyScope
                , envSourceRef = startRef
                , envSynRep = ""
                }
-         
+
+
+data Stack = Stack
+    { stackPointer :: StackFrame
+    , stackTop :: StackFrame
+    , stackStack :: [StackFrame]
+    } deriving (Show, Eq)
+           
+data StackFrame = StackFrame
+    { frameName :: String
+    } deriving (Show, Eq)
+                
+
+-- In a language like Java, could use try..catch
+-- to exit early from function, maybe something
+-- similar for this?
+
+-- For this,
+-- Throw the 'error' in the return statement,
+-- then catch it in the calling expression.
